1fad967a8f80b4047665cb9befb8efee321bc9b9
Philipp Wendler <uni@philippwendler.de>
2017-10-02 14:07:50 +0200

    Update setup.py, documentation, and copyright.

--- reprounzip-containerexec/setup.py
+++ reprounzip-containerexec/setup.py
@@ -1,42 +1,45 @@
 import io
 import os
-from setuptools import setup
-
+import setuptools
 
 # pip workaround
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-
-# Need to specify encoding for PY3, which has the worst unicode handling ever
 with io.open('README.rst', encoding='utf-8') as fp:
-    description = fp.read()
-req = [
-    'reprounzip>=1.0.8',
-    'rpaths>=0.8',
-    'benchexec>=1.10']
-setup(name='reprounzip-containerexec',
-      version='0.1',
-      author = 'Dirk Beyer',
-      description="Linux tool enabling reproducible experiments (unpacker) in an isolated environment",
-      url='https://github.com/sosy-lab/reprozip',
-      packages=['reprounzip', 'reprounzip.unpackers'],
-      entry_points={
-          'reprounzip.unpackers': [
-              'containerexec = reprounzip.unpackers.containerexec:setup']},
-      namespace_packages=['reprounzip', 'reprounzip.unpackers'],
-      install_requires=req,
-      long_description=description,
-      license='BSD-3-Clause',
-      keywords=['reprozip', 'reprounzip', 'reproducibility', 'provenance',
-                'vida', 'nyu', 'benchexec', 'containerexec', 'container'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Topic :: Scientific/Engineering',
-          'Topic :: System :: Archiving'])
+    long_description = fp.read()
+setuptools.setup(
+    name = 'reprounzip-containerexec',
+    version = '1.0',
+    author = 'Dirk Beyer',
+    description = "An unpacker for reprozip using the container technology of BenchExec",
+    long_description = long_description,
+    url = 'https://www.reprozip.org',
+    license = 'BSD-3-Clause',
+    keywords = ['reprozip', 'reprounzip', 'reproducibility', 'provenance',
+            'vida', 'nyu', 'benchexec', 'containerexec', 'container'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Archiving'],
+    platforms = ['Linux'],
+
+    packages = ['reprounzip', 'reprounzip.unpackers'],
+    entry_points = {
+        'reprounzip.unpackers': [
+            'containerexec = reprounzip.unpackers.containerexec:setup']
+        },
+    namespace_packages = ['reprounzip', 'reprounzip.unpackers'],
+    install_requires = [
+        'reprounzip>=1.0.8',
+        'rpaths>=0.8',
+        'benchexec>=1.11',
+        ],
+    zip_safe = True,
+    )
